3b75cedc8191b7300eed0e059f2a6fef2fdb4d2d
blackawn <2877116178@qq.com>
2023-09-10 01:02:59 +0800

    update

---
+++ src/layout/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { NLayout } from 'naive-ui'
+
+import Header from './header/index.vue'
+import Aside from './aside/index.vue'
+import Main from './main/index.vue'
+import Footer from './footer/index.vue'
+
+</script>
+<template>
+  <NLayout>
+    <div class="flex h-screen flex-col">
+      <Header />
+      <div class="flex flex-1">
+        <Aside />
+        <Main />
+      </div>
+      <Footer />
+    </div>
+  </NLayout>
+</template>
+<style>
+</style>
