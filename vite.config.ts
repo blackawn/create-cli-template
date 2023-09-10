0955653a8e885feddc21295fe23c7ce8b96a445e
blackawn <2877116178@qq.com>
2023-09-10 13:21:40 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import * as path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  envPrefix: 'VITE_'
+export default defineConfig((config) => {
+
+  const env = loadEnv(config.mode, process.cwd())
+
+  return {
+    plugins: [
+      react()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API_URL,
+          ws: true,
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, '/')
+        },
+      }
+    },
+    esbuild:{
+      drop: ['console', 'debugger']
+    },
+    envPrefix: 'VITE_'
+  }
 })
