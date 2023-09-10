d4fbbf8181795df85ff45a3ae75bd3b3e9b439ba
blackawn <2877116178@qq.com>
2023-09-10 13:28:16 +0800

    update

--- src/router/RouterGuard.tsx
+++ src/router/RouterGuard.tsx
@@ -1,15 +1,7 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
 export default function RouterGuard() {
-  const navigate = useNavigate()
-
-  const [init, setInit] = useState(false)
-
-  useEffect(() => {
-    navigate('/layout/home')
-    setInit(true)
-  }, [navigate])
 
   return (
     <Outlet />
